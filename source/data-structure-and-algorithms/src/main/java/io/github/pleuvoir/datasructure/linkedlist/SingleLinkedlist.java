0c1b0960b5183285a73759c1891695e45e8f2057
pleuvoir <pleuvior@foxmail.com>
2019-02-22 22:39:50 +0800

    :wrench: 修改链表

--- source/data-structure-and-algorithms/src/main/java/io/github/pleuvoir/datasructure/linkedlist/SingleLinkedlist.java
+++ source/data-structure-and-algorithms/src/main/java/io/github/pleuvoir/datasructure/linkedlist/SingleLinkedlist.java
@@ -19,14 +19,12 @@ public class SingleLinkedlist {
     // 在尾部添加数据
     public void add(Object o) {
         Node newNode = new Node(o);
-
         // 从头部开始遍历找到最后一个并赋值
         Node headNodeAdd = head;
 
         while (headNodeAdd.next != null) {
             headNodeAdd = headNodeAdd.next;
         }
-
         headNodeAdd.next = newNode;
         size++;
     }
@@ -45,26 +43,20 @@ public class SingleLinkedlist {
 
     // 根据下标访问节点
     public Node getByIndex(int index) {
-
         if (index < 0 || index > size - 1) {
             return null;
         }
-
         Node p = this.head;
-
         int pos = 0;
         while (p.next != null && index >= pos) {
             p = p.next;
             pos++;
         }
-
         return p;
-
     }
 
     // 删除第一个结点
     public Object removeFirst() {
-
         Node headToUse = head;
         if (headToUse.next != null) {
             final Object second = headToUse.next.data;  // 首节点后的第一个元素
@@ -72,7 +64,6 @@ public class SingleLinkedlist {
 
             return second;
         }
-
         size--;
         return null;
     }
@@ -80,32 +71,116 @@ public class SingleLinkedlist {
 
     public Object removeLast() {
         if (size == 0) return null;
-
         // 寻找尾结点
         Node lastNode = head;
-
         while (lastNode.next != null) {
             lastNode = lastNode.next;
         }
-
         // 找到倒数第二个结点 设置 next 为 null
         Node prevLastNode = head;
-
         // 从头开始遍历，停止时 prevLastNode 即为倒数第二个结点
         while (prevLastNode.next != null && prevLastNode.next != lastNode) {
             prevLastNode = prevLastNode.next;
         }
-
         prevLastNode.next = null;
         size--;
-
         return lastNode.data;
     }
 
 
+    // 单链表反转
+    public void reverse() {
+
+        Node current = this.head.next;
+        Node temp = null;   // 临时变量用于保存下一个需要切换的节点地址
+        Node dummy = null;
+
+        while (current != null) {
+            temp = current.next;
+
+            /**
+             *  假设数据为 [1,2]
+             *  第一次循环 Node1->next = null
+             *  第二次循环 Node2->next = Node1(Node1->next = null)
+             */
+
+            current.next = dummy;   // 每次遍历将下一个节点设置为上一个操作过的元素
+            dummy = current;  // 下次遍历时 dummy 即为上一个操作过的元素
+
+            current = temp;
+        }
+        this.head.next = dummy;
+    }
+
+
+    // 删除链表倒数第 n 个结点
+    public void deleteNodeByReverseIndex(int index) {
+
+        if (this.size == 0 || this.size < index) {
+            return;
+        }
+        // 先将倒数转为正数索引
+        int i = this.size - index ;
+
+        Node p = this.head;
+
+        int pos = 0;
+        while (p.next != null && i != pos) {
+            p = p.next;
+            pos++;
+        }
+        // 此时 p 即为要删除的节点，接下里寻找上一个节点
+        Node prevP = head;
+
+        // 从头开始遍历，停止时 prevLastNode 即为倒数第二个结点
+        while (prevP.next != null && prevP.next != p) {
+            prevP = prevP.next;
+        }
+
+        // 此时 prevP 为要删除节点的上一个节点，移动指针
+        prevP.next = p.next;
+        p = null; // help gc
+    }
+
+
+    // 求链表的中间结点，使用快慢指针此种解法较为巧妙
+    public Node middle() {
+        Node p = this.head;
+        Node middle = this.head.next;
+        while (p.next != null && p.next.next != null) {
+            p = p.next.next;
+            middle = middle.next;
+        }
+        return middle;
+    }
+
+    // 链表中环的检测
+    public boolean checkCircle() {
+
+        if (size == 0) return false;
+
+        Node fast = this.head.next;  // 使用快慢指针
+        Node slow = this.head.next;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == null) {
+                // 已经到达末尾停止。如果存在环则不会停止，需要判断快慢指针相遇后停止
+                return false;
+            } else if (fast == slow) {
+                System.out.println("快慢指针相遇.. 是环状链表");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Data
     public static class Node {
-        Node next;
-        Object data;
+        private Node next;
+        private Object data;
 
         public Node(Object data) {
             this.data = data;
--- source/data-structure-and-algorithms/src/test/java/datastructure/array/SingleLinkedlistTest.java
+++ source/data-structure-and-algorithms/src/test/java/datastructure/array/SingleLinkedlistTest.java
@@ -39,5 +39,54 @@ public class SingleLinkedlistTest {
         System.out.println("list.getByIndex(2)=" + list.getByIndex(2));  // Node{data=4}
 
 
+        list.printAll();
+        // 删除倒数第四个结点
+        list.deleteNodeByReverseIndex(4);
+
+        list.printAll();
+
+        new SingleLinkedlist().deleteNodeByReverseIndex(2);
+
+
+        // 反转链表
+
+        SingleLinkedlist test3 = new SingleLinkedlist();
+        test3.add(1);
+        test3.add(2);
+     //   test3.add(3);
+
+        test3.printAll();
+        test3.reverse();
+
+        test3.printAll();
+
+
+        test3.add(3);
+        test3.add(4);
+        test3.add(5);
+        test3.add(6);
+        test3.add(7);
+        test3.printAll();
+        SingleLinkedlist.Node middle = test3.middle();
+        System.out.println(middle);
+
+
+        // 环测试
+
+        SingleLinkedlist circleContainer = new SingleLinkedlist();
+        circleContainer.add(1);
+        circleContainer.add(2);
+        circleContainer.add(3);
+        circleContainer.add(4);
+
+        // 设置最后一个节点指向第一个
+        SingleLinkedlist.Node last = circleContainer.getByIndex(3);
+
+        SingleLinkedlist.Node first = circleContainer.getByIndex(0);
+
+        last.setNext(first);
+
+
+        System.out.println(circleContainer.checkCircle());
     }
 }
